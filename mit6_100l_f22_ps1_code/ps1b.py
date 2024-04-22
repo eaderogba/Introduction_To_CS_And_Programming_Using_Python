457ebcc32d2c35ca29b7c6af4dcfc9cfc7014f73
eaderogba <eaderogba279@stu.ui.edu.ng>
2024-04-22 22:15:32 +0100

    Update

--- mit6_100l_f22_ps1_code/ps1b.py
+++ mit6_100l_f22_ps1_code/ps1b.py
@@ -2,7 +2,6 @@
 ## Name:
 ## Time Spent:
 ## Collaborators:
-import time
 
 ##########################################################################################
 ## Get user input for yearly_salary, portion_saved, cost_of_dream_home, semi_annual_raise below ##
@@ -11,9 +10,6 @@ yearly_salary = float(input("Enter your yearly salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 cost_of_dream_home = float(input("Enter the cost of your dream home: "))
 semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
-
-# Record start time
-start_time = time.time()
 
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
@@ -27,17 +23,11 @@ months = 0
 ###############################################################################################
 ## Determine how many months it would take to get the down payment for your dream home below ## 
 ###############################################################################################
-"""Here, the program loops until the"""
-while amount_saved <= cost_of_down_payment:
+
+while amount_saved < cost_of_down_payment:
     amount_saved += (yearly_salary / 12 * portion_saved) + (amount_saved * r / 12)
     if months % 6 == 0 and months != 0:
         yearly_salary = yearly_salary + (yearly_salary * semi_annual_raise)
     months += 1
 
 print(f"At this rate, it will take you {months} to save up for the down payment")
-
-# Record the end time
-end_time = time.time()
-# Calculate elapsed time
-elapsed_time = end_time - start_time
-print(f"Time taken: {elapsed_time:.5f} seconds")
--- mit6_100l_f22_ps1_code/ps1c.py
+++ mit6_100l_f22_ps1_code/ps1c.py
@@ -6,14 +6,45 @@
 ##############################################
 ## Get user input for initial_deposit below ##
 ##############################################
-
-
+initial_deposit = float(input("Enter the initial deposit: "))
+portion_down_payment = 0.25
+cost_of_dream_home = 800000
+cost_of_down_payment = cost_of_dream_home * portion_down_payment
+months = 36
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
-#########################################################################
-
+######################################################################### 
+lower_bound = 0
+upper_bound = 1
+tolerance = 100
+steps = 0
 
 ##################################################################################################
 ## Determine the lowest rate of return needed to get the down payment for your dream home below ##
 ##################################################################################################
+while steps < 36:
+    # Calculates the rate midpoint
+    r = (lower_bound + upper_bound) / 2
 
+    # Determines the amount of savings needed
+    savings_needed = initial_deposit * (1 + r / 12) ** months
+
+    # Detrmines if the savings is withing the tolerance
+    if abs(savings_needed - cost_of_down_payment) <= tolerance:
+        print(f"Savings amount achieved within tolerance: {savings_needed:.2f}")
+        break
+    elif savings_needed < cost_of_down_payment:
+        # There is then a need to increase the rate, by shifting towards the upper bound i.e.,
+        lower_bound = r
+    else:
+        # The rate decreases towards the lower bound
+        upper_bound = r
+    # Increase steps
+    steps += 1
+
+    # This checks if the amount is not achievable within the tolerance. In this case r is set to None
+    if savings_needed < cost_of_down_payment - tolerance and steps == 36:
+        r = None
+
+print(f"Steps taken: {steps}")
+print(f"Minimum rate of return needed: {r:.4f}" if r is not None else "Best savings rate: None \nCannot achieve savings goal")
